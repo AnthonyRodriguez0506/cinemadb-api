bd3fb4872f17d675b7becebc650a7ecbf40a1735
Anthony Rodríguez <anthonyrodriguez0506@outlook.com>
2026-03-24 20:03:52 -0400

    chore: initialize Spring Boot project structure for cinemadb-api

---
+++ src/main/java/com/anthony/cinemadb/api/CinemadbApiApplication.java
@@ -0,0 +1,13 @@
+package com.anthony.cinemadb.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemadbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemadbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anthony/cinemadb/api/CinemadbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthony.cinemadb.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemadbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
